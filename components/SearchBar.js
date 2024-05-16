dbf23bed4dafdb5e0a2109333348dcfcc8ead788
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-05-16 19:14:34 +0200

    create first html skeleton for searchbar

---
+++ components/SearchBar.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function SearchBar () {
+    return (
+        <div>
+            <input></input>
+            <button></button>
+        </div>
+    );
+};
+
+export default SearchBar;
