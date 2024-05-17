09becc5250387d18585ab9f9b62be453f186bf56
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-05-17 15:48:41 +0200

    create empty playlist component

---
+++ src/components/playlist.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Playlist () {
+    return (
+        <div>
+            {/* список песен которые добавлены в новый плейлис */}
+        </div>
+    );
+}
