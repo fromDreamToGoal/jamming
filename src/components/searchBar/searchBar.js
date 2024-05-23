a8f39777ceee548a10ed12a544dc427ef5a16618
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-05-23 06:12:34 +0200

    create function for get user's access token

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,10 +1,35 @@
 import './searchBar.css';
+import { useEffect } from 'react';
+
+
 
 function SearchBar () {
+
+    useEffect(() => {
+        document.getElementById('authorize-button').addEventListener('click', getToken);
+        return () => {
+            document.getElementById('authorize-button').removeEventListener('click', getToken);
+        }; 
+    }, []);
+
+    const getToken = (event) => {
+        event.preventDefault();
+        const clientId = '8b57f561cd76450194bfd65bf89333e6';
+        const redirectUri = 'http://localhost:3000/?';
+        const responseType = 'token';
+        const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+        const authUrl = `${authEndpoint}?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}`;
+        console.log(authUrl);
+        document.getElementById('authorize-button').addEventListener('click', () => {
+            window.location = authUrl;
+        });
+    };
+
     return (
-        <form className="main-box">
-            <input className='input-field' type="text"></input>
-            <button className='button'>Search</button>
+        <form className="main-box" >
+            <input className='input-field' type="text" ></input>
+            <button className='button' type='submit' id='authorize-button'>Search</button>
        </form>
     );
 };
