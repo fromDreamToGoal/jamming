57f6d549af7d80798076d7247c67422f7b8730be
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-05-24 15:35:40 +0200

    create method for get access token from url

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -6,10 +6,12 @@ import { useEffect } from 'react';
 function SearchBar () {
 
     useEffect(() => {
-        document.getElementById('authorize-button').addEventListener('click', getToken);
-        return () => {
-            document.getElementById('authorize-button').removeEventListener('click', getToken);
-        }; 
+        const token = getAccessTokenFromURL();
+        if(token) {
+            console.log('Access token: ', token);
+        } else {
+            console.log('Token not found');
+        };
     }, []);
 
     const getToken = (event) => {
@@ -21,13 +23,33 @@ function SearchBar () {
 
         const authUrl = `${authEndpoint}?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}`;
         console.log(authUrl);
-        document.getElementById('authorize-button').addEventListener('click', () => {
-            window.location = authUrl;
-        });
+
+        window.location = authUrl;
+        
+    };
+
+    function getAccessTokenFromURL() {
+        const hash = window.location.hash;
+        if (!hash) {
+            return null;
+        }
+
+        const params = new URLSearchParams(hash.substring(1));
+        const accessToken = params.get('access_token');
+        const expiresIn = params.get('expires_in');
+
+        if(accessToken) {
+            window.location.hash = '';
+
+            localStorage.setItem('access_token', accessToken);
+            setTimeout( () => localStorage.removeItem('access_token'), expiresIn * 1000);
+
+            return accessToken;
+        }
     };
 
     return (
-        <form className="main-box" >
+        <form className="main-box" onSubmit={getToken}>
             <input className='input-field' type="text" ></input>
             <button className='button' type='submit' id='authorize-button'>Search</button>
        </form>
