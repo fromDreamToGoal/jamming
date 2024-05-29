983ba0cfeccb29228814332ccca6b91c112bdb42
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-05-29 06:27:42 +0200

    create functions for handling button

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,21 +1,42 @@
 import './searchBar.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 function SearchBar () {
+    const [inputValue, setInputValue] = useState('');
+    const [accessToken, setAccessToken] = useState(null);
 
     useEffect(() => {
-        const token = getAccessTokenFromURL();
-        if(token) {
-            console.log('Access token: ', token);
+        localStorage.removeItem('access_token');
+
+        const tokenFromURL = getAccessTokenFromURL();
+        const tokenFromStorage = localStorage.getItem('access_token');
+        if (tokenFromURL) {
+            console.log('Access token from URK: ', tokenFromURL);
+            setAccessToken(tokenFromURL);
+        } else if (tokenFromStorage) {
+            console.log('Access token from srorage: ', tokenFromStorage);
+            setAccessToken(tokenFromStorage);
         } else {
             console.log('Token not found');
-        };
+        }
     }, []);
 
-    const getToken = (event) => {
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleButtonClick = (event) => {
         event.preventDefault();
+        if (!accessToken) {
+            getToken();
+        } else {
+            handleSearch();
+        }
+    };
+
+    const getToken = () => {
         const clientId = '8b57f561cd76450194bfd65bf89333e6';
         const redirectUri = 'http://localhost:3000/?';
         const responseType = 'token';
@@ -26,7 +47,23 @@ function SearchBar () {
 
         window.location = authUrl;
         
-    };
+    }
+
+    const handleSearch = async () => {
+        const endpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(inputValue)}&type=track`;
+        const response = await fetch(endpoint, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Search results: ', data);
+        } else {
+            console.error('Failed to fetch search results');
+        }
+    }
 
     function getAccessTokenFromURL() {
         const hash = window.location.hash;
@@ -41,16 +78,20 @@ function SearchBar () {
         if(accessToken) {
             window.location.hash = '';
 
-            localStorage.setItem('access_token', accessToken);
-            setTimeout( () => localStorage.removeItem('access_token'), expiresIn * 1000);
+            sessionStorage.setItem('access_token', accessToken);
+            setTimeout( () => sessionStorage.removeItem('access_token'), expiresIn * 1000);
 
             return accessToken;
         }
+        return null;
     };
 
     return (
-        <form className="main-box" onSubmit={getToken}>
-            <input className='input-field' type="text" ></input>
+        <form className="main-box" onSubmit={handleButtonClick}>
+            <input className='input-field'
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}></input>
             <button className='button' type='submit' id='authorize-button'>Search</button>
        </form>
     );
