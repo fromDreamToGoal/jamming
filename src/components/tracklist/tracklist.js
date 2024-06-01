51f82aabb6d5cad7cf4ed4e4bddec522fa55be33
Serhii Holovenko <sergei.golovenko@icloud.com>
2024-06-01 23:13:57 +0200

    update style, delete album rendering in results

--- src/components/tracklist/tracklist.js
+++ src/components/tracklist/tracklist.js
@@ -7,14 +7,14 @@ const TrackList = ({ tracks }) => {
       {tracks.length === 0 ? (
         <p>No tracks found</p>
       ) : (
-        <ul>
+        <ul className='table'>
           {tracks.map(track => (
             <li key={track.id} className="track-item">
                 <img src={track.album.images[0]?.url} alt={track.album.name} width="50" height="50" />
                 <div className="track-details">
                   <p>{track.name}</p>
                   <p>{track.artists.map(artist => artist.name).join(', ')}</p>
-                  <p>{track.album.name}</p>
+                  {/* <p>{track.album.name}</p> */}
                 </div>
                 <button className='button-add'>+</button>
             </li>
